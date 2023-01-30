a5fd34036201fa87c11ea978849c6d312a15454f
DESKTOP-A2R6AE6\Djordjije <djordjijeradovic24@gmail.com>
2023-01-30 15:46:14 +0100

    designed table for employees;

--- LibraryApp.Server/src/forms/ServerEmployeesForm.java
+++ LibraryApp.Server/src/forms/ServerEmployeesForm.java
@@ -1,6 +1,10 @@
 package forms;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.util.List;
+import javax.swing.JTable;
+import javax.swing.table.TableColumnModel;
 import tcp.TcpServer;
 
 /**
@@ -15,9 +19,30 @@ public class ServerEmployeesForm extends javax.swing.JDialog {
     public ServerEmployeesForm(java.awt.Frame parent, boolean modal, List<TcpServer> tcpServerList) {
         super(parent, modal);
         initComponents();
+        pack();
+        getContentPane().setBackground(new Color(150,194,215));
+        setLocationRelativeTo(null);
         tblEmployees.setModel(new EmployeesTableModel(tcpServerList));
+        designTable();
     }
-
+    private void designTableColumns(){
+        tblEmployees.setAutoResizeMode(JTable.AUTO_RESIZE_NEXT_COLUMN);
+        TableColumnModel columnModel = tblEmployees.getColumnModel();
+        columnModel.getColumn(0).setPreferredWidth(60);    
+        columnModel.getColumn(1).setPreferredWidth(160);
+        columnModel.getColumn(2).setPreferredWidth(160);
+        columnModel.getColumn(3).setPreferredWidth(400);
+    }
+    private void designTable(){
+        tblEmployees.getTableHeader().setOpaque(false);
+        tblEmployees.getTableHeader().setBackground(new Color(107,158,183));
+        tblEmployees.getTableHeader().setForeground(Color.white);
+        tblEmployees.getTableHeader().setFont(new Font("Cascadia Code", Font.PLAIN, 14));
+        tblEmployees.setBackground(new Color(217,238,255));
+        designTableColumns();
+    }
+    
+    
     public void fireTableChange(){
         ((EmployeesTableModel)tblEmployees.getModel()).fireTableDataChanged();
     }
@@ -36,6 +61,12 @@ public class ServerEmployeesForm extends javax.swing.JDialog {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
+        tblEmployees.setBackground(new java.awt.Color(217, 238, 255));
+        tblEmployees.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED, new java.awt.Color(204, 204, 255), new java.awt.Color(204, 204, 255)));
+        tblEmployees.setFont(new java.awt.Font("Cascadia Code", 0, 14)); // NOI18N
+        tblEmployees.setFillsViewportHeight(true);
+        tblEmployees.setGridColor(new java.awt.Color(153, 204, 255));
+        tblEmployees.setSelectionBackground(new java.awt.Color(217, 238, 255));
         jScrollPane1.setViewportView(tblEmployees);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -44,7 +75,7 @@ public class ServerEmployeesForm extends javax.swing.JDialog {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 550, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 551, Short.MAX_VALUE)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
